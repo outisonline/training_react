f13a3faa7654be5abc9fdd339faef6fe30f952e9
YokouchiShoki <yokouchishoki@YokouchiShokinoMacBook-Pro.local>
2021-08-26 09:48:49 +0900

    init commit

---
+++ src/components/Square.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { ISquare } from '../interface';
+
+interface SquareProps {
+    value: ISquare;
+    onClick: () => void;
+  }
+  
+  function Square(props:SquareProps) {
+    return (
+      <button className="square" onClick={props.onClick}>
+        {props.value}
+      </button>
+    );
+  }
+  
+
+export default Square;
---
+++ src/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Game from './components/Game'
+import './index.css'
+// ========================================
+ReactDOM.render(<Game />, document.getElementById('root'));
